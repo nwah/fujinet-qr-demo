a2b09d2504d4e44607bdfcb39dcf9089948b8792
Noah Burney <noah.burney@gmail.com>
2025-02-23 10:17:47 -0600

    hook up apple2 commands

--- src/apple2/qr.c
+++ src/apple2/qr.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <string.h>
 #include <conio.h>
 #include <6502.h>
@@ -12,7 +13,7 @@
 #define FUJICMD_QRCODE_LENGTH              0xBE
 #define FUJICMD_QRCODE_OUTPUT              0xBF
 
-uint8_t qrData[1024];
+static uint8_t qrData[1024];
 
 bool fuji_qr_input(char *s, uint16_t len)
 {
@@ -75,9 +76,6 @@ bool fuji_qr_set_output_mode(uint8_t output_mode) {
 
 bool fuji_qr_length(uint16_t *length) {
   // send STAT command: FUJICMD_QRCODE_LENGTH
-  // PAYLOAD:
-  // 0,1 : length
-  // 2   : Output mode (0-3)
   if (sp_get_fuji_id() == 0) {
     return false;
   }
@@ -87,7 +85,9 @@ bool fuji_qr_length(uint16_t *length) {
     return false;
   }
 
-  length = (uint16_t *)sp_payload[2];
+  memcpy(length, sp_payload, 2);
+  // *length = sp_payload[0];
+  // *length = sp_payload[1];
 
   return true;
 }
@@ -106,93 +106,21 @@ bool fuji_qr_output(uint8_t *output, uint16_t len) {
     return false;
   }
 
-  memcpy(output, &sp_payload[2], len);
+  // memcpy(output, &sp_payload[2], len);
+  memcpy(output, &sp_payload[0], 441);
 
   return true;
 }
 
-#define VERSION 3
-#define SIZE VERSION * 4 + 17
-
-static const char qr[SIZE][SIZE] =
- {{"*******  *    * ***** *******"},
-  {"*     * ** *****    * *     *"},
-  {"* *** *  ***  ***  ** * *** *"},
-  {"* *** * ** * * *****  * *** *"},
-  {"* *** *   * ***  **** * *** *"},
-  {"*     * * *    ***  * *     *"},
-  {"******* * * * * * * * *******"},
-  {"            * * *  *         "},
-  {"***** ***** * ****   * * * * "},
-  {"  * ** * *    * *  ** ***   *"},
-  {" *  ***  * *******   **      "},
-  {"*        ***  **   **  * * * "},
-  {" *** **  * * ***** *     **  "},
-  {" ** *    ** *  ** ** ** *   *"},
-  {" *** ***     ***  * * ** **  "},
-  {"** **   *   * ***   * ***  * "},
-  {"    *** *** * ** *     * **  "},
-  {"* **    * *  ** *  ** *** * *"},
-  {"*  *  * *****  **      *  *  "},
-  {"*   *   ** *   ** **   **  * "},
-  {"*   * * *  * ** **  ***** ***"},
-  {"        *** *     * *   *****"},
-  {"******* * *  * * **** * ***  "},
-  {"*     *   * * * *  **   *   *"},
-  {"* *** * * * * ****  ***** ***"},
-  {"* *** * **    * ****   * ****"},
-  {"* *** * ** **  ** **  ****** "},
-  {"*     * *  *  * * ***  * * * "},
-  {"******* ** ** * **** * ****  "}};
-
-// static uint8_t bitmap[SIZE * SIZE / 8 + SIZE];
-static uint8_t bitmap[1024];
-
-/**
-* to_bitmap - Convert QR code to compact binary format
-*
-* Replaces data in out_buf, with data suitable for copying directly into
-* bitmap graphics memory. Each QR module (pixel) is represented by 1 bit.
-* Most significant bit is leftmost pixel. The last bits of the final byte
-* of each row are returned as 0s. A 21x21 QR code will be 63 bytes (3 bytes
-* per row of 21 bits (= 24 bits with 3 unused) * 21 rows).
-*/
-
-static uint8_t *qr_to_bitmap(uint8_t size) {
-  uint8_t x, y;
-  uint8_t* p = bitmap;
-
-  bzero(bitmap, sizeof(bitmap));
-
-  for (y = 0; y < size; ++y) {
-    for (x = 0; x < size; ++x) {
-      if (qr[y][x] == '*')
-        *p |= 1;
-      if (x % 8 != 7)
-        *p <<= 1;
-      else
-        ++p;
-    }
-    *p <<= 7 - x % 8;
-    ++p;
-  }
-
-  return bitmap;
-}
-
-/////////////////////////////////////////////////////////////////////////
-// everything above this comment is supposed to be done on the FujiNet //
-/////////////////////////////////////////////////////////////////////////
-
 #pragma static-locals(on)
 
 bool qr_encode_text(char *text, uint8_t version, uint8_t ecc, bool shorten, uint8_t output_mode, char *result) {
-  uint8_t size = VERSION * 4 + 17;
+  uint8_t size = version * 4 + 17;
   uint8_t offset = (40 - size) / 2; // center
   uint8_t x, y;
   uint16_t length;
   struct regs regs;
-  register uint8_t* bitmap;
+  register uint8_t* bitmap = &qrData[0];
 
   // to be replaced by getting the bitmap from the FujiNet
   // bitmap = qr_to_bitmap(size);
@@ -201,7 +129,9 @@ bool qr_encode_text(char *text, uint8_t version, uint8_t ecc, bool shorten, uint
   fuji_qr_encode(version, ecc, shorten);
   fuji_qr_set_output_mode(output_mode);
   fuji_qr_length(&length);
-  fuji_qr_output(bitmap, length);
+  // printf("length: %d", length);
+  // fuji_qr_output(bitmap, length);
+  fuji_qr_output(qrData, size * size);
 
   regs.pc = 0xFB40; // SETGR
   _sys(&regs);
